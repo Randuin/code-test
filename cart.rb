b072ffee6dbe6a48efb422071ac06e4a408e0b69
kinsbrunner <kinsbrunner@gmail.com>
2016-11-23 07:53:32 -0500

    Adding tax into Item model. Refactoring Cart total method, implementing subtotal method.

--- cart.rb
+++ cart.rb
@@ -1,9 +1,10 @@
 class Item
-  attr_reader :name, :price
+  attr_reader :name, :price, :tax
 
-  def initialize(name, price)
+  def initialize(name, price, tax = 0)
     @name = name
     @price = price
+    @tax = tax
   end
 end
 
@@ -13,16 +14,27 @@ class Cart
   end
 
   def total
-    @items.map(&:price).inject(&:+)
+    subtotal + tax
   end
 
   def subtotal
+    @items.map(&:price).inject(&:+)
   end
 
   def tax
+    @items.inject(0) {|total_tax, item| total_tax + (item.price * (item.tax / 100.0)) }
   end
 
   def add_item(item)
     @items << item
   end
 end
+
+cart = Cart.new
+item = Item.new('Zanahorias', 60, 10.5) # tax = 6.3
+cart.add_item(item)
+item = Item.new('Zanahorias', 10, 1) # tax = 0.2
+cart.add_item(item)
+puts "Tax: #{cart.tax}"            # 6.4
+puts "Sub-total: #{cart.subtotal}" # 70
+puts "Total: #{cart.total}"        # 81
