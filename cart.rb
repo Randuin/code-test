822906db136b90c928b6af54bfd7b04f4ddfb611
Jazz Garcha <jaspreet@garcha.com>
2016-11-23 09:13:01 -0800

    update tests, update subtotal, tax, total methods, and update item initialize

--- cart.rb
+++ cart.rb
@@ -1,25 +1,33 @@
 class Item
-  attr_reader :name, :price
+  attr_reader :name, :price, :tax
 
-  def initialize(name, price)
+  def initialize(name, price, product_type)
     @name = name
     @price = price
+    #I put some thought into the product_type parameter, I wanted it to be reusable.
+    #I thought about passing in the tax amount or product type. As more product
+    #types are added, this will change into a case statement, generally becoming
+    #much more complicated. However atm I needed to not tax produces.
+    @tax = product_type == 'produce' ? 0 : price/10
   end
 end
 
 class Cart
+
   def initialize
     @items = []
   end
 
   def total
-    @items.map(&:price).inject(&:+)
+    subtotal + tax
   end
 
   def subtotal
+    @items.map(&:price).inject(&:+)
   end
 
   def tax
+    @items.map(&:tax).inject(&:+)
   end
 
   def add_item(item)
--- cart_spec.rb
+++ cart_spec.rb
@@ -1,26 +1,35 @@
 require './cart'
+require 'pry'
 
 RSpec.describe Cart do
-  let(:item) { Item.new('Carrot', 100) }
+  let(:carrot) { Item.new('Carrot', 100, 'produce') }
+  let(:coke) { Item.new('Coke', 100, 'soda') }
+  let(:cart) { Cart.new }
 
   describe '#subtotal' do
-    it 'returns the total amount from the cart without tax' do
-      cart = Cart.new
-      cart.add_item(item)
-      cart.add_item(item)
-      expect(cart.total).to eq(200)
+    it 'returns the subtotal amount from the cart without tax' do
+      cart.add_item(coke)
+      cart.add_item(coke)
+      expect(cart.subtotal).to eq(200)
     end
   end
 
   # 1) Implement the code that passes this test, assume tax rate of 10%
   describe '#total' do
     it 'returns the total amount from the cart with tax' do
-      cart = Cart.new
-      cart.add_item(item)
-      cart.add_item(item)
-      expect(cart.subtotal).to eq(220)
+      cart.add_item(coke)
+      cart.add_item(coke)
+      expect(cart.total).to eq(220)
     end
   end
 
   # 2) Implement the idea that some items are produce and do not incur tax and write the tests that relate to this behavior. Feel free to change anything, add methods, delete methods, add classes, delete classes.
+  describe '#tax' do
+    it 'returns the tax amount from the cart with tax for some items, and without tax for others' do
+      cart.add_item(coke)
+      cart.add_item(coke)
+      cart.add_item(carrot)
+      expect(cart.total).to eq(320)
+    end
+  end
 end
