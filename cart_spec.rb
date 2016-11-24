a6794be9bd2f6e32f954db3606df8f5c4c5e68f0
Devin Starks <DevStarks@gmail.com>
2016-11-24 15:48:27 -0500

    writes tests for #tax in Item and Produce classes

--- cart_spec.rb
+++ cart_spec.rb
@@ -4,15 +4,15 @@ RSpec.describe Item do
   describe '#tax' do
     context 'when no tax rate is passed' do
       it 'defaults to returning 10% of the price' do
-        general_item = Item.new('Mop', 350)
-        expect(general_item.tax).to eq(35)
+        item = Item.new('Mop', 350)
+        expect(item.tax).to eq(35)
       end
     end
 
     context 'when tax rate is passed' do
       it 'returns tax rate multiplied by price' do
-        general_item = Item.new('Mop', 350, 0.2)
-        expect(general_item.tax).to eq(70)
+        item = Item.new('Mop', 350, 0.2)
+        expect(item.tax).to eq(70)
       end
     end
   end
@@ -28,39 +28,35 @@ RSpec.describe Produce do
 end
 
 RSpec.describe Cart do
-  let(:produce_item) { Produce.new('Carrot', 100) }
-  let(:general_item) { Item.new('Mop', 350) }
+  let(:produce_item) { Produce.new('Orange', 150) }
+  let(:general_item) { Item.new('Broom', 350) }
 
-  let(:mixed_cart) do
+  describe '#add_item' do
+    it 'correctly adds items to cart' do
+      cart = Cart.new
+      cart.add_item(produce_item)
+      cart.add_item(general_item)
+      expect(cart.items).to contain_exactly(produce_item, general_item)
+    end
+  end
+
+  let(:cart) do
     cart = Cart.new
     cart.add_item(produce_item)
     cart.add_item(general_item)
     cart
   end
 
-  let(:non_produce_cart) do
-    cart = Cart.new
-    cart.add_item(general_item)
-    cart.add_item(general_item)
-    cart
-  end
-
-  describe '#items' do
-    it 'returns all items that are added to cart' do
-      expect(mixed_cart.items).to contain_exactly(produce_item, general_item)
-    end
-  end
-
   describe '#subtotal' do
     it 'returns the total amount from the cart without tax' do
-      expect(mixed_cart.subtotal).to eq(450)
+      expect(cart.subtotal).to eq(500)
     end
   end
 
   describe '#total' do
     it 'return the sum of the cart subtotal and tax' do
-      total = mixed_cart.subtotal + mixed_cart.tax
-      expect(mixed_cart.total).to eq(total)
+      total = cart.subtotal + cart.tax
+      expect(cart.total).to eq(total)
     end
   end
 end
